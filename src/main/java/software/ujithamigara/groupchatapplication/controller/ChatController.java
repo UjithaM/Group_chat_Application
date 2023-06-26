193ec45abc5d8f76fb0f438dea645737c89f103d
uji100 <97239721+uji100@users.noreply.github.com>
2023-06-26 13:25:57 +0530

    set up the server

--- src/main/java/software/ujithamigara/groupchatapplication/controller/ChatController.java
+++ src/main/java/software/ujithamigara/groupchatapplication/controller/ChatController.java
@@ -6,15 +6,24 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class ChatController {
     @FXML
     private AnchorPane node;
 
     @FXML
-    private JFXTextArea txtArea;
+    private  JFXTextArea txtArea;
+
 
     @FXML
     private JFXTextField txtFieldMassage;
+    Socket socket;
+    DataOutputStream dataOutputStream;
+    DataInputStream dataInputStream;
 
     @FXML
     void sendBtnOnAction(ActionEvent event) {
@@ -26,7 +35,17 @@ public class ChatController {
 
     }
     public void initialize(){
-        
+        new Thread(()->{
+            try {
+                socket = new Socket("localhost", 3004);
+                dataInputStream =  new  DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
     }
 
 }
