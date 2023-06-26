07932994c24e292c3616a3c56783f1be5bea4867
uji100 <97239721+uji100@users.noreply.github.com>
2023-06-26 10:30:18 +0530

    add textArea and text Field also actions for send button and txtFied

--- src/main/java/software/ujithamigara/groupchatapplication/controller/ChatController.java
+++ src/main/java/software/ujithamigara/groupchatapplication/controller/ChatController.java
@@ -1,5 +1,32 @@
 package software.ujithamigara.groupchatapplication.controller;
 
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class ChatController {
+    @FXML
+    private AnchorPane node;
+
+    @FXML
+    private JFXTextArea txtArea;
+
+    @FXML
+    private JFXTextField txtFieldMassage;
+
+    @FXML
+    void sendBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtFiledMassageOnAction(ActionEvent event) {
+
+    }
+    public void initialize(){
+        
+    }
 
 }
