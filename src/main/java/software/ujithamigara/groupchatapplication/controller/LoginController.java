6f1c18725d9d1f9b2731896dfa1fc1904e230e75
uji100 <97239721+uji100@users.noreply.github.com>
2023-06-26 10:58:00 +0530

    Add an initialize method that creates a new thread, sets up the server socket, accepts client connections, and includes the data input stream and output stream

--- src/main/java/software/ujithamigara/groupchatapplication/controller/LoginController.java
+++ src/main/java/software/ujithamigara/groupchatapplication/controller/LoginController.java
@@ -5,6 +5,12 @@ import javafx.fxml.FXML;
 import com.jfoenix.controls.JFXTextField;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class LoginController {
 
     @FXML
@@ -21,5 +27,19 @@ public class LoginController {
     void loginOnAction(ActionEvent event) {
 
     }
+    public void initialize(){
+        new Thread(()-> {try {
+            ServerSocket serverSocket = new ServerSocket(3002);
+            Socket socket = serverSocket.accept();
 
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }}).start();
+    }
 }
